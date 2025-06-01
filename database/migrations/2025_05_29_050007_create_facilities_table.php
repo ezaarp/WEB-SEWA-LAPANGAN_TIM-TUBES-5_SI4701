46b71eaff57df2341123e3c1f1c77cae28706e69
azzkaaaaa <azkaalmulki@student.telkomuniversity.ac.id>
2025-06-01 21:53:22 +0700

    buat tabel fasilitas, aprovals

---
+++ database/migrations/2025_05_29_050007_create_facilities_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('facilities', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignId('area_id')->constrained()->onDelete('cascade');
+            $table->foreignId('facility_type_id')->constrained()->onDelete('cascade');
+            $table->enum('status', ['available', 'unavailable'])->default('available');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('facilities');
+    }
+};
