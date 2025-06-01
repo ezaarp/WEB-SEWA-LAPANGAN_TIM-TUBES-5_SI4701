031b019a62b36b774b6b71c37e194429ed86a70a
boodibadoobu <nailvinz@gmail.com>
2025-06-01 21:35:29 +0700

    migration database

---
+++ database/migrations/2025_05_29_050010_create_payments_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('booking_id')->constrained()->onDelete('cascade');
+            $table->decimal('amount', 10, 2)->default(20000); // Default payment amount
+            $table->string('payment_proof')->nullable(); // File path for payment proof
+            $table->enum('status', ['belum_dibayar', 'dibayar', 'gagal'])->default('belum_dibayar');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
